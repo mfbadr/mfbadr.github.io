453456050bcb3a76ec9252c384f766ee1232307a
Mikey Badr <mfbadr@gmail.com>
2014-11-06 10:19:05 -0600

    added skills and portfolio pages

---
+++ client/views/portfolio/portfolio.js
@@ -0,0 +1,10 @@
+(function(){
+  'use strict';
+
+  angular.module('mfbadr')
+  .controller('PortfolioCtrl', ['$scope', function($scope){
+    $scope.title = 'Portfolio'
+
+
+  }]);
+})();
--- index.js
+++ index.js
@@ -6,6 +6,8 @@
   .config(['$routeProvider', function($routeProvider){
     $routeProvider
     .when('/', {templateUrl:'/views/home/home.html', controller:'HomeCtrl'})
+    .when('/skills', {templateUrl:'/views/skills/skills.html', controller:'SkillsCtrl'})
+    .when('/portfolio', {templateUrl:'/views/portfolio/portfolio.html', controller:'PortfolioCtrl'})
     .otherwise({redirectTo:'/'});
 
   }]);
