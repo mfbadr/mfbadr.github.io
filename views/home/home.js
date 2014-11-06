d4a53156e26a3bcedb94439e3f2cb45c30396090
Mikey Badr <mfbadr@gmail.com>
2014-11-06 00:37:24 -0600

    idea generator, fixed link

--- views/home/home.js
+++ views/home/home.js
@@ -3,7 +3,54 @@
 
   angular.module('mfbadr')
   .controller('HomeCtrl', ['$scope', function($scope){
-    $scope.title = 'MIKEYBADR';
+    $scope.firstList = [
+      'Uber',
+      'Tinder',
+      'Facebook',
+      'Craigslist',
+      'Yelp',
+      'eBay',
+      'Bitcoin',
+      'Reddit',
+      'Youtube',
+      'Instagram',
+      'Spotify',
+      'Netflix',
+      'Games',
+      'A blog',
+      'Portfolios',
+      'A store'
+      ];
+
+    $scope.secondList = [
+      'puppies',
+      'hipsters',
+      'vegans',
+      'travelers',
+      'artists',
+      'kittens',
+      'teenagers',
+      'memes',
+      'bros',
+      'gamers',
+      'board games',
+      'cosplayers',
+      'facial hair',
+      'unicorns',
+      'video games',
+      'musicians',
+      'politicians',
+      'hackers'
+    ];
+
+    $scope.first = _.sample($scope.firstList);
+    $scope.second = _.sample($scope.secondList);
+
+    $scope.newIdea = function(){
+      $scope.first = _.sample($scope.firstList);
+      $scope.second = _.sample($scope.secondList);
+    };
+
 
   }]);
 })();
